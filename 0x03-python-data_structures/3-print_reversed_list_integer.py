c09a420a0339beebb6354f6d4c242dbbbcb95153
sunday0prime <sundayprime@gmail.com>
2023-03-14 18:13:49 -0700

    ALX python data structures assignment

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/bin/usr/python3
 
 def print_reversed_list_integer(my_list=[]):
-    my_list.sort(reverse=True)
-    [print("{:d}".format(x) for x in my_list]
+    
+    if my_list:
+        for i in reversed(my_list):
+            print("{:d}:.format(i))
