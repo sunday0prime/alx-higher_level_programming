868eba28a01ed7136e49f6838f8d3de0afe19b31
sunday0prime <sundayprime@gmail.com>
2023-03-17 17:20:54 -0700

    0x04 python more data structures

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    result = {}
+    for x in sorted(list(a_dictionary.keys())):
+        result[x] = a_dictionary[x]
+    return result
